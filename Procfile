95693cc4af1421289ffaf04db5ce1ee507439f90
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-28 08:14:52 +0100

    Created procfile, added allowed hosts and set debug to False for initial deployment test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn letsTalkMoney.wsgi
--- letsTalkMoney/settings.py
+++ letsTalkMoney/settings.py
@@ -28,9 +28,13 @@ import env
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
