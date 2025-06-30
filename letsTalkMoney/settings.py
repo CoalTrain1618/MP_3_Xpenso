8f56caa0014e042bfce0cb935e726424ff812340
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-30 14:26:21 +0100

    removed custom crispyform from sign in & added standard post form. Added custom signup.html

--- letsTalkMoney/settings.py
+++ letsTalkMoney/settings.py
@@ -131,6 +131,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # TO CHANGE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -154,3 +156,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
