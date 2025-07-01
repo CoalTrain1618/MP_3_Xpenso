b37c6e4ab686a7b3e30d808dd10e914c521df6de
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-01 12:43:13 +0100

    Created accounts profile.html and added profile_view to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
+# makes login the landing page.
 def landing_login(request):
     return redirect('account_login')
+
+
+#
+def profile_view(request):
+    profile = request.user.profile
+    return render(request, 'profile.html', {'profile': profile})
