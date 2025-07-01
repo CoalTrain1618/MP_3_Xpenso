ff713260d2d806a5692802c70e4421dd6d2e917f
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-01 09:38:20 +0100

    Added signals.py with to link existing Users with profiles.

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
+    def __init__(self):
+        return f"{self.user.username}'s profile"
