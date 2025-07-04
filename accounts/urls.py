60d40825730953ecf5bde02630af66462e73e84a
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-04 16:41:55 +0100

    Added delete_user view and url path for future delete_user.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
+    path('delete/', views.delete_user, name='delete_user'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -12,3 +12,14 @@ def landing_login(request):
 def profile_view(request):
     profile = request.user.profile
     return render(request, 'profile/profile.html', {'profile': profile})
+
+# view to delete user account
+def delete_user(request):
+    user = request.user
+    if user.is_authenticated:
+        if request.method == "POST":
+            user.delete()
+            logout(request)
+            return redirect('account_login')
+        else:
+            return render(request, 'profile/delete_user.html')
