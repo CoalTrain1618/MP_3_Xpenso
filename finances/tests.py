58de9bdf12a7f09ce68a6857d04d31c5f637c0bf
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-07 10:25:36 +0100

    Added example data for automated tests

--- finances/tests.py
+++ finances/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Budget, Income, Category, Expenses
+
+User = get_user_model()
 
 # Create your tests here.
+
+class FinanceModelsTest(TestCase):
+
+    #create and define example data to use in all tests
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser42", password="testpass")
+        self.budget = Budget.objects.create(user_id=self.user, 
+                                            amount=1000, month=7, year=2025)
+        self.category = Category.objects.create(user_id=self.user, name="Groceries")
+
+    
