8aba1b72781d500bc285d1bfd45be0f30e771755
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-07 08:37:36 +0100

    Added Budget model to finances/models.py

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Budget(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_set = models.DateField(auto_now_add=True)
+    month = models.PositiveSmallIntegerField()
+    year = models.PositiveSmallIntegerField()
