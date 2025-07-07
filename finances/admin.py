24811bd30bd477d84d1626791906af9a2a4efb9f
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-07 09:52:44 +0100

    Added models to admin.py

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Budget, Income, Category, Expenses
 
 # Register your models here.
+
+admin.site.register(Budget)
+admin.site.register(Income)
+admin.site.register(Category)
+admin.site.register(Expenses)
