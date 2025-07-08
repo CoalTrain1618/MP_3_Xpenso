c7c2aa14f2246549f2ca3d8d874ea43e66d30022
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-08 13:09:10 +0100

    Added budget form. Removed init from form as not needed

--- finances/forms.py
+++ finances/forms.py
@@ -2,13 +2,11 @@ from django import forms
 from django.forms import ModelForm
 from .models import Budget, Income, Category, Expenses
 
+#Budget form for user to create budget.
 class BudgetForm(ModelForm):
     class Meta:
         model = Budget
         fields = ['amount', 'month', 'year']
 
-    def __init__(self,*args,**kwargs):
-        user = kwargs.pop('user')
-        super().__init__(*args, **kwargs)
-        self.fields['Budget'].queryset = Budget.objects.filter()
-        
+
+#Income form for user to create Income
