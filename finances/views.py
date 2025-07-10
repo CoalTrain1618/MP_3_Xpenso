4edc9e9c81ab40c0784ca75eab32041cf6f10c5f
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-07-10 06:30:29 +0100

    Added start of BudgetView

--- finances/views.py
+++ finances/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Budget, Income, Category, Expenses
+from .forms import BudgetForm, IncomeForm, ExpenseForm
 
 # Create your views here.
+
+class BudgetView(LoginRequiredMixin ,CreateView):
+    """
+    This budget view allows the user to submit budget data.
+    The user will be redirected to dashboard on success.
+    """
+    model = Budget
+    form_class = BudgetForm
+    template_name = 'finances/budget_form.html'
+    success_url = reverse_lazy('dashboard')
+    
